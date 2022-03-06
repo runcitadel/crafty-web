0161a0ca8670202bfe0859f572e9ad917e02457f
Aaron Dewes <aaron.dewes@protonmail.com>
2022-03-06 19:32:00 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Download the latest minecraft vanilla server to /servers/vanilla.jar
-wget -O /servers/vanilla.jar https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar
+wget -O /servers/vanilla.jar https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar
 
 # Download spigot too
 wget -O /servers/spigot.jar https://download.getbukkit.org/spigot/spigot-1.18.1.jar
